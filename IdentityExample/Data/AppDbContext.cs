289efe772e58bad5ab5aa07fc107d6dbdd258da1
shreyaaa08 <89118427+shreyaaa08@users.noreply.github.com>
2021-08-18 09:44:26 +0530

    Add files via upload

---
+++ IdentityExample/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace IdentityExample.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
