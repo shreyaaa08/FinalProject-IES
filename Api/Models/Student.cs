9bb23e542621c595c0f1e37f19844bdd97165a1b
shreyaaa08 <89118427+shreyaaa08@users.noreply.github.com>
2021-08-19 00:51:34 +0530

    Update Student.cs

--- Api/Models/Student.cs
+++ Api/Models/Student.cs
@@ -11,6 +11,5 @@ namespace Api.Models
         public string Lastname { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string Token { get; set; }
     }
 }
