fa5639db7cff0b0b71327dfb4f3279565b1ab671
shreyaaa08 <89118427+shreyaaa08@users.noreply.github.com>
2021-08-18 09:34:55 +0530

    Add files via upload

---
+++ Api/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System;
+
+namespace Api.Controllers
+{
+    #region StudentController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private List<Student> students = new List<Student>()
+            {
+                new Student{StudentId =1, Firstname = "SHREYA", Lastname="SADANANDAN", UserName="shreya", Password ="123456"},
+                new Student{StudentId =2, Firstname = "RIYA", Lastname="DESAI", UserName="riya", Password ="123456"},
+                new Student{StudentId =3, Firstname = "ROSHEL", Lastname="VARGHESE", UserName="roshel", Password ="123456"},
+                new Student{StudentId =4, Firstname = "AKSHATA", Lastname="HEGDE", UserName="akshata", Password ="123456"},
+                new Student{StudentId =5, Firstname = "JUHI", Lastname="MANIKPURKAR", UserName="juhi", Password ="123456"}
+            };
+
+        // GET: api/AllStudents
+
+        // GET: api/Student
+        [HttpGet]
+        [Authorize]
+        public List<Student> Get()
+        {
+            return students;
+        }
+
+        // GET: api/Student/5
+        [Authorize]
+        [HttpGet("{id}", Name = "Get")]
+        public Student Get(int id)
+        {
+            Student s = null;
+            students.ForEach(delegate (Student student)
+            {
+                if (student.StudentId == id)
+                {
+                    s = student;
+                }
+            });
+            return s;
+        }
+    }
+}
+#endregion
---
+++ Api/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Api.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+}
